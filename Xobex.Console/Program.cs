8ee10dbabcc74788798ab430e7990c18b7890cbe
Dmitry Kolchev <dmitrykolchev@msn.com>
2026-08-26 20:45:59 +0300

    fix

--- Xobex.Console/Program.cs
+++ Xobex.Console/Program.cs
@@ -18,77 +18,52 @@ internal class Program
             var conOut = WindowsOutputAdapter.Create();
             using var conIn = WindowsInputAdapter.Create();
             using var mouse = conIn.EnableMouseInput();
-            var terminalParser = conIn.CreateParser();
-            for (; ; )
-            {
-                if (terminalParser.TryGetInputEvent(out var ev))
-                {
-                    conOut.WriteLine($"{ev}");
-                    conOut.Flush();
-                    if (ev?.EventType == InputEventType.Key)
-                    {
-                        if (ev.Key.Key == ConsoleKey.C && ev.Key.Mod == ConsoleModifiers.Control)
-                        {
-                            conOut.WriteLine($"Ctrl-C pressed. Exiting");
-                            conOut.Flush();
-                            break;
-                        }
-                        else if (ev.Key.Ch == 'M')
-                        {
-                            conIn.EnableMouseInput();
-                            conOut.WriteLine($"Mouse input enabled");
-                            conOut.Flush();
-                        }
-                        else if (ev.Key.Ch == 'm')
-                        {
-                            conIn.DisableMouseInput();
-                            conOut.WriteLine($"Mouse input disabled");
-                            conOut.Flush();
-                        }
-                    }
-                }
-            }
+            ProcessConsoleEvents(conIn, conOut);
         }
-        else if(OperatingSystem.IsLinux())
+        else if (OperatingSystem.IsLinux())
         {
             var conOut = LinuxOutputAdapter.Create();
             using ITerminalInputAdapter conIn = LinuxInputAdapter.Create(conOut);
             using var mouse = conIn.EnableMouseInput();
-            var terminalParser = conIn.CreateParser();
-
-            for (; ; )
-            {
-                if (terminalParser.TryGetInputEvent(out var ev))
-                {
-                    conOut.WriteLine($"{ev}");
-                    conOut.Flush();
-                    if (ev?.EventType == InputEventType.Key)
-                    {
-                        if (ev.Key.Key == ConsoleKey.C && ev.Key.Mod == ConsoleModifiers.Control)
-                        {
-                            conOut.WriteLine($"Ctrl-C pressed. Exiting");
-                            conOut.Flush();
-                            break;
-                        }
-                        else if (ev.Key.Ch == 'M')
-                        {
-                            conIn.EnableMouseInput();
-                            conOut.WriteLine($"Mouse input enabled");
-                            conOut.Flush();
-                        }
-                        else if (ev.Key.Ch == 'm')
-                        {
-                            conIn.DisableMouseInput();
-                            conOut.WriteLine($"Mouse input disabled");
-                            conOut.Flush();
-                        }
-                    }
-                }
-            }
+            ProcessConsoleEvents(conIn, conOut);
         }
         else
         {
             throw new NotSupportedException("OS not supported");
+        }
+    }
+
+    private static void ProcessConsoleEvents(ITerminalInputAdapter conIn, ITerminalOutputAdapter conOut)
+    {
+        var terminalParser = conIn.CreateParser();
+        for (; ; )
+        {
+            if (terminalParser.TryGetInputEvent(out var ev))
+            {
+                conOut.WriteLine($"{ev}");
+                conOut.Flush();
+                if (ev?.EventType == InputEventType.Key)
+                {
+                    if (ev.Key.Key == ConsoleKey.C && ev.Key.Mod == ConsoleModifiers.Control)
+                    {
+                        conOut.WriteLine($"Ctrl-C pressed. Exiting");
+                        conOut.Flush();
+                        break;
+                    }
+                    else if (ev.Key.Ch == 'M' && ev.Key.KeyDown)
+                    {
+                        conIn.EnableMouseInput();
+                        conOut.WriteLine($"Mouse input enabled");
+                        conOut.Flush();
+                    }
+                    else if (ev.Key.Ch == 'm' && ev.Key.KeyDown)
+                    {
+                        conIn.DisableMouseInput();
+                        conOut.WriteLine($"Mouse input disabled");
+                        conOut.Flush();
+                    }
+                }
+            }
         }
     }
 }
